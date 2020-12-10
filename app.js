3c50e01ae9868e1cfadeb3aa12d84c79d2de241f
lutfi rama dhanu <lutfiramadhanu_tkj1@binusasmg.sch.id>
2020-12-10 15:57:58 +0700

    hasil

--- app.js
+++ app.js
@@ -1,54 +1,63 @@
 const express = require('express');
 const mysql = require('mysql');
 const hbs = require('hbs');
-const bodyParser = require('body-parser');
-
-
+const bodyParser =  require('body-parser');
 
 const app = express();
-const port = 7500;
+const port = 1900;
 
-// view engine hbs
-app.set('view egine', 'hbs');
+app.set('view engine', 'hbs');
 
-//setting parser data dari mysql ke indexjs
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-const koneksi = mysql.createConnection({
+var koneksi = mysql.createConnection({
     host: 'localhost',
     user: 'lutfi',
     password: '0000',
-    database: 'My_TODO'
+    database: 'data_penduduk'
 });
-
 koneksi.connect((err) => {
     if(err) throw err;
-    console.log("koneksi database berhasil disambungkan");
-})
+    console.log("koneksi database berhasil disambung");
+});
 
 app.get('/', (req, res) => {
-    koneksi.query('SELECT*FROM aktivitas', (err, hasil) => {
+    koneksi.query('SELECT * FROM data_penduduk', (err, hasil) => {
         if(err) throw err;
-        res.render('home.hbs',{
-            judulhalaman: 'ACTIVITAS',
+        res.render('home.hbs', {
+            judulhalaman: 'DATA_PENDUDUK',
             data: hasil
         });
     });
 });
 
-
-app.post('/tambahkegiatan', (req, res) =>{
-    var detailkegiatan = req.body.inputdetailkegiatan;
-    var tanggal = req.body.inputtanggal;
-    koneksi.query('INSERT INTO aktivitas(detail_kegiatan, tanggal)values(?,?)',
-    [detailkegiatan, tanggal],
+app.post('/penduduk', (req, res) =>{
+    var nama = req.body.inputnama;
+    var alamat = req.body.inputalamat;
+    var TTL = req.body.inputTTL;
+    var golongan_darah = req.body.inputgolongan_darah;
+    var telepon = parseInt(req.body.inputtelepon);
+    koneksi.query('INSERT INTO data_penduduk(nama, alamat, TTL, golongan_darah, telepon) values(?,?,?,?,?)',
+    [nama, alamat, TTL, golongan_darah, telepon],
     (err, hasil) => {
         if(err) throw err;
         res.redirect('/');
     }
     )
+});    
+
+app.get('/hapus-nama/:nama', (req, res) => {
+        var nama = req.params.nama; 
+        koneksi.query('DELETE FROM data_penduduk WHERE nama=?', 
+        [nama], (err, hasil) => {
+            if(err) throw err;
+            res.redirect('/');
+        }
+    )
 });
-app.listen(port, () => {
-    console.log(`app MY_TODO berjalan pada port ${port}`);
-});
+ 
+
+app.listen(port ,() => {
+    console.log(`app berjalan pada port ${port}`);
+}); 
